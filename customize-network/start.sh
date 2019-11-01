c11299244d6d7b4af2271e27d490a60df952ad5c
H M Shahriar <h.m.shahriar@konasl.com>
2019-11-01 19:34:39 +0600

    update config for multi peer

--- customize-network/start.sh
+++ customize-network/start.sh
@@ -12,7 +12,7 @@ export MSYS_NO_PATHCONV=1
 
 docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d ca.konai.com konaorderer.konai.com kslpeer1.orgkona.konai.com couchdb
+docker-compose -f docker-compose.yml up -d ca.konai.com konaorderer.konai.com kslpeer1.orgkona.konai.com kslpeer2.orgkona.konai.com couchdb
 docker ps -a
 
 # wait for Hyperledger Fabric to start
@@ -25,3 +25,10 @@ sleep ${FABRIC_START_TIMEOUT}
 docker exec -e "CORE_PEER_LOCALMSPID=OrgKonaMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@orgkona.konai.com/msp" kslpeer1.orgkona.konai.com peer channel create -o konaorderer.konai.com:7050 -c konachannel -f /etc/hyperledger/configtx/channel.tx
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=OrgKonaMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@orgkona.konai.com/msp" kslpeer1.orgkona.konai.com peer channel join -b konachannel.block
+
+docker ps -a
+
+docker exec -e "CORE_PEER_LOCALMSPID=OrgKonaMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@orgkona.konai.com/msp" -e "CORE_PEER_ADDRESS=kslpeer2.orgkona.konai.com:8051" kslpeer2.orgkona.konai.com peer channel fetch 0 konachannel.block -c konachannel -o konaorderer.konai.com:7050
+
+# Join peer1.org1.example.com to the channel.
+docker exec -e "CORE_PEER_LOCALMSPID=OrgKonaMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@orgkona.konai.com/msp" -e "CORE_PEER_ADDRESS=kslpeer2.orgkona.konai.com:8051" kslpeer2.orgkona.konai.com peer channel join -o konaorderer.konai.com:7050 -b ./konachannel.block
